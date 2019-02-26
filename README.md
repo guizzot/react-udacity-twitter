e4a2c987b514dc433f8d5c17cc15271623827ad6
Guilherme Ribeiro <43709413+guizzot@users.noreply.github.com>
2019-02-26 20:28:34 -0300

    Initial Commit
    Project finished.

---
+++ README.md
@@ -0,0 +1,14 @@
+# Chirper Project
+
+This repo is a code-along with the first project in the [React Nanodegree program](https://www.udacity.com/course/react-nanodegree--nd019).
+
+Most of the commits in this repository correspond to videos in the program.
+
+## Project Setup
+
+* clone the Project
+* install the dependencies - `npm install`
+
+## License
+
+MIT
---
+++ src/components/App.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { handleInitialData } from '../actions/shared'
+import Dashboard from './Dashboard'
+import LoadingBar from 'react-redux-loading'
+import NewTweet from './NewTweet'
+import TweetPage from './TweetPage'
+import Nav from './Nav'
+
+class App extends Component {
+  
+  componentDidMount(){
+    this.props.dispatch(handleInitialData())
+  }
+
+  render() {
+    return (
+      <Router>
+         <React.Fragment>
+            <LoadingBar />
+            <div className='container'>
+              <Nav />
+              {
+                this.props.loading === true
+                ? null
+                : <React.Fragment>
+                    <Route path='/' exact component={Dashboard} />
+                    <Route path='/tweet/:id' component={TweetPage} />
+                    <Route path='/new' component={NewTweet} />
+                    
+                  </React.Fragment>
+              }
+            </div>
+        </React.Fragment>
+      </Router>
+     
+    )
+  }
+}
+
+function mapStateToProps({authedUser}){
+  return {
+    loading: authedUser === null
+  }
+}
+
+export default connect(mapStateToProps)(App)
